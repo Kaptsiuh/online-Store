1e386c8bec0de1fa9ea00c051c0a54cf68ca73d9
Denis Kovalev <kovvdn@gmail.com>
2022-12-31 16:42:34 +0300

    init: initialize tailwind.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import { Outlet } from 'react-router-dom'
 
 const App: FC = () => {
   return (
-    <>
-      <header>Header</header>
-      <main>
-        <Outlet />
+    <div className="min-h-screen flex flex-col">
+      <header>
+        <div className="container">Header</div>
+      </header>
+      <main className="flex-1">
+        <div className="container">
+          <Outlet />
+        </div>
       </main>
-      <footer>footer</footer>
-    </>
+      <footer>
+        <div className="container">footer</div>
+      </footer>
+    </div>
   )
 }
 
