b5e264e8ee8e805f3561280b28560000a82fd480
Dima <dimaberry1@gmail.com>
2023-01-03 23:48:19 +0300

    feat: add responsive and new styles on product page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -20,7 +20,7 @@ const ProductPage: FC = () => {
 
   const images = product.images.map((path) => (
     <img
-      className=" w-11/12 m-2 rounded-md"
+      className="w-11/12 m-2 rounded-md max-md:w-[13%] hover:opacity-75 cursor-pointer"
       src={path}
       alt="product-photo"
       key={path}
@@ -30,8 +30,10 @@ const ProductPage: FC = () => {
 
   return (
     <div className="container">
-      <div className="flex justify-between m-6 text-xl">
-        <Link to="/">Store</Link>
+      <div className="flex justify-between m-6 text-xl max-md:text-xs">
+        <Link to="/" className="hover:text-orange-500">
+          Store
+        </Link>
         <span>/</span>
         {product.category}
         <span>/</span>
@@ -40,49 +42,53 @@ const ProductPage: FC = () => {
         {product.title}
       </div>
       <div className="bg-white rounded-xl shadow-xl mb-4">
-        <div className="text-center bg-slate-300 rounded-t-xl">
+        <div className="text-center bg-gray-600 text-white rounded-t-xl">
           <h1 className="py-2 text-xl">{product.title}</h1>
         </div>
-        <div className="flex">
-          <div className=" w-2/12 rounded-bl-xl">{images}</div>
-          <div className=" flex justify-center items-center w-5/12">{mainImage}</div>
-          <div className="flex flex-col  w-5/12 rounded-br-xl">
+        <div className="flex max-md:flex-col max-md:items-center">
+          <div className=" w-2/12 rounded-bl-xl max-md:flex max-md:w-full max-md:justify-start">
+            {images}
+          </div>
+          <div className=" flex justify-center items-center w-5/12 max-md:items-center max-md:w-full">
+            {mainImage}
+          </div>
+          <div className="flex flex-col  w-5/12 rounded-br-xl max-md:w-full">
             <div className=" m-2 flex justify-center items-center flex-col">
               <h2 className="text-3xl">{product.price}$</h2>
 
               <button
                 onClick={!isInCart ? () => addToCart(product) : () => dropFromCart(product)}
-                className="bg-slate-400 hover:bg-orange-500 m-4 p-2 w-full rounded-md"
+                className="bg-gray-800 text-white uppercase hover:bg-orange-500 m-4 p-2 w-full rounded-md"
               >
                 {isInCart ? 'Drop From Cart' : 'Add To Cart'}
               </button>
 
-              <button className="bg-slate-400 hover:bg-orange-500 p-2 w-full rounded-md">
+              <button className="bg-gray-800 text-white uppercase hover:bg-orange-500 p-2 w-full rounded-md">
                 BUY NOW
               </button>
             </div>
             <h2 className="bg-gray-100 mt-4 m-2 rounded-md text-center">
-              <p className="bg-slate-300 rounded-t-md">Description:</p>
+              <p className="bg-gray-400 rounded-t-md">Description:</p>
               {product.description}
             </h2>
             <h2 className="bg-gray-100 m-2 rounded-md text-center">
-              <p className="bg-slate-300 rounded-t-md">Discount Percentage:</p>
+              <p className="bg-gray-400 rounded-t-md">Discount Percentage:</p>
               {product.discountPercentage}
             </h2>
             <h2 className="bg-gray-100 m-2 rounded-md text-center">
-              <p className="bg-slate-300 rounded-t-md">Rating:</p>
+              <p className="bg-gray-400 rounded-t-md">Rating:</p>
               {product.rating}
             </h2>
             <h2 className="bg-gray-100 m-2 rounded-md text-center">
-              <p className="bg-slate-300 rounded-t-md">Stock:</p>
+              <p className="bg-gray-400 rounded-t-md">Stock:</p>
               {product.stock}
             </h2>
             <h2 className="bg-gray-100 m-2 rounded-md text-center">
-              <p className="bg-slate-300 rounded-t-md">Brand:</p>
+              <p className="bg-gray-400 rounded-t-md">Brand:</p>
               {product.brand}
             </h2>
             <h2 className="bg-gray-100 mb-4 m-2 rounded-md text-center">
-              <p className="bg-slate-300 rounded-t-md">Category:</p>
+              <p className="bg-gray-400 rounded-t-md">Category:</p>
               {product.category}
             </h2>
           </div>
