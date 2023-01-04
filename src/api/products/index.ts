4fbf3757196b8b0e8e1b4a6e4da928d92556621e
ds-kavaliou <ds.kavaliou@gmail.com>
2023-01-04 10:11:31 +0300

    upd: api layer to sort and filter

--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -1,8 +1,10 @@
 import { request } from '../request'
-import type { Product } from '../../types'
+import { SortBy } from '../../types'
+import type { FilterOptions, Product } from '../../types'
 
 type ResponseDTO = {
   products: Product[]
+  categories: string[]
 }
 
 const API_URL = 'https://dummyjson.com'
@@ -19,16 +21,61 @@ const fetchData = async (): Promise<ResponseDTO> => {
   )
 
   cache.products = products
+  cache.categories = [...new Set(products.map((item) => item.category))]
 
   return cache
 }
 
-export const fetchProducts = async (): Promise<Product[]> => {
+export const fetchProducts = async (options: Partial<FilterOptions>): Promise<Product[]> => {
+  const { sort, ...filtering } = options
   const { products } = await fetchData()
-  return products
+
+  const filtered = Object.entries(filtering).reduce(
+    (products, [key, value]) =>
+      products.filter(
+        (item) => filterPredicates[key as keyof FilterOptions]?.call(null, item, value) ?? true
+      ),
+    [...products]
+  )
+
+  if (sort) {
+    filtered.sort(sortByPredicates[sort?.toString() as SortBy])
+  }
+
+  return filtered
 }
 
 export const fetchProduct = async (id: Product['id']): Promise<Product | null> => {
   const { products } = await fetchData()
   return products.find((item) => item.id === id) ?? null
+}
+
+export const fetchCategories = async (): Promise<string[]> => {
+  const { categories } = await fetchData()
+  return categories
+}
+
+const sortByPredicates: {
+  [key: string]: (itemA: Product, itemB: Product) => number
+} = {
+  [SortBy.priceASC]: (itemA, itemB) => itemA.price - itemB.price,
+  [SortBy.priceDESC]: (itemA, itemB) => itemB.price - itemA.price,
+  [SortBy.ratingASC]: (itemA, itemB) => itemA.rating - itemB.rating,
+  [SortBy.ratingDESC]: (itemA, itemB) => itemB.rating - itemA.rating,
+}
+
+const filterPredicates: {
+  [key in keyof Partial<FilterOptions>]: (item: Product, value: string[]) => boolean
+} = {
+  category: (item: Product, value: string[]) =>
+    value.some((v) => v.toLowerCase() === item.category.toLowerCase()),
+  brand: (item: Product, value: string[]) =>
+    value.some((v) => v.toLowerCase() === item.brand.toLowerCase()),
+  price: (item: Product, value: string[]) =>
+    item.price >= Number(value[0]) && item.price <= Number(value[1]),
+  stock: (item: Product, value: string[]) =>
+    item.stock >= Number(value[0]) && item.stock <= Number(value[1]),
+  search: (item: Product, value: string[]) =>
+    item.title.toLowerCase().includes(value.toString().toLowerCase()) ||
+    item.description.toLowerCase().includes(value.toString().toLowerCase()),
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,32 @@
-type ID = number;
+type ID = number
 
 export type Product = {
-  id: ID;
-  title: string;
-  description: string;
-  price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  brand: string;
-  category: string;
-  thumbnail: string;
-  images: string[];
-};
+  id: ID
+  title: string
+  description: string
+  price: number
+  discountPercentage: number
+  rating: number
+  stock: number
+  brand: string
+  category: string
+  thumbnail: string
+  images: string[]
+}
+
+export type FilterOptions = {
+  search: string[]
+  sort: string[]
+  category: string[]
+  brand: string[]
+  size: string[]
+  price: string[]
+  stock: string[]
+}
+
+export enum SortBy {
+  priceASC = 'price-ASC',
+  priceDESC = 'price-DESC',
+  ratingASC = 'rating-ASC',
+  ratingDESC = 'rating-DESC',
+}
