8b38d936aa83d2bef71a972bf697bdbc5c5d408e
ds-kavaliou <ds.kavaliou@gmail.com>
2023-01-04 11:41:07 +0300

    feat: add sass suport

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 
-import './index.css'
+import './index.scss'
+
 import CartProvider from './context/CartContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
