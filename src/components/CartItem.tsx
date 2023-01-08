9f2327baaf9842bcdbf2e124a3adf49a79b56841
ds-kavaliou <ds.kavaliou@gmail.com>
2023-01-08 22:37:53 +0300

    refactor: upd CartItem

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { FC, ReactNode } from "react";
 import { IoAdd, IoRemove } from "react-icons/io5";
 import { Link } from "react-router-dom";
@@ -17,26 +18,34 @@ const CartItem: FC<CartItemProps> = (props) => {
     <div key={product.id} className="flex flex-col md:flex-row md:space-x-4 items-center space-y-4 md:space-y-0 px-4 py-2 border">
       {info}
       <Link to={`/products/${product.id}`} className="flex-1">
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+        <div className="flex flex-col md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-4">
           <img
             src={product.thumbnail}
             alt={product.title}
-            className="h-28 md:h-20 aspect-video object-cover"
+            className="h-28 aspect-video object-cover"
           />
-          <div>
+          <div className="flex-1">
             <h1 className="text-xl font-medium">{product.title}</h1>
-            <p>{product.description}</p>
+            <p className="mb-2">{product.description}</p>
+            <div className="text-gray-600 text-sm font-medium flex items-center justify-between md:flex-col md:items-start">
+              <span>Rating: {product.rating}</span>
+              <span>Discount: {product.discountPercentage}</span>
+            </div>
           </div>
         </div>
       </Link>
-      <div className="flex items-center space-x-4">
-        <button onClick={onAdd}>
-          <IoAdd />
-        </button>
-        <div>{count}</div>
-        <button onClick={onRemove}>
-          <IoRemove />
-        </button>
+      <div className="flex flex-col items-center gap-y-1">
+        <div className="capitalize text-sm">stock: <span className="font-medium">{product.stock}</span></div>
+        <div className="flex items-center space-x-4">
+          <button className={clsx(count >= product.stock && "pointer-events-none")} onClick={onAdd}>
+            <IoAdd />
+          </button>
+          <div className="fony-[2ch]">{count}</div>
+          <button onClick={onRemove}>
+            <IoRemove />
+          </button>
+        </div>
+        <div className="capitalize text-sm">price: <span className="font-medium">{product.price * count}$</span></div>
       </div>
     </div>
   );
